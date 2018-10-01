255610c2c097f6e5055d93b35c67d066d13a466a
Annette Hong <annetteh@andrew.cmu.edu>
2018-10-01 11:57:33 -0400

    asdfsadf

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function addNewList() {
 }
 
 function addListItem() {
+    console.log('hello world');
     var list = document.getElementById("grocery-list");
     var itemInput = document.getElementById("new-list-item");
     var newItem=document.createElement("li");
@@ -11,7 +12,7 @@ function addListItem() {
 }
 
 function deleteListItem(item){
-    item.parentNode.removeChild(item);
+    item.remove();
 }
 
 function completeListItem(item){
@@ -21,3 +22,35 @@ function completeListItem(item){
         item.parentNode.style.textDecoration="none";
     }
 }
+
+/*$(document).ready(function(){
+    $("#add-item").click(function() { // bind handler for click event
+        var list = $("#grocery-list"); // get the ol list by id
+        var itemInput = $("#new-list-item"); // get the new item input
+      // append the input value within an li element
+        list.append("<li>" + itemInput.val() + "<button class='delete-item'>X</button></li>")
+    });
+    $(".delete-item").click(function() {
+        console.log("test");
+        $(this).parent().remove();
+    });
+});*/
+
+$(document).on("click", ".delete-item", function() {
+    $(this).parent().remove();
+});
+
+$(document).on("click", "#add-item", function() {
+    var list = $("#grocery-list");
+    var itemInput = $("#new-list-item");
+    list.append("<li>" + "<input type='checkbox' class='complete-item'>" + itemInput.val() + " <button class='delete-item'>X</button></li>");
+});
+
+$(document).on("click", ".complete-item", function() {
+    if($(this).is(':checked')) {
+        $(this).parent().css('text-decoration', 'line-through');
+    } else {
+        $(this).parent().css('text-decoration', 'none');
+    }
+});
+
